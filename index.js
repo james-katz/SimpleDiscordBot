e707d90067b905e870e1d32917c1c29249288725
James Katz <james.j.katz@protonmail.com>
2022-10-26 20:10:11 -0300

    deferReply bigfix

--- index.js
+++ index.js
@@ -18,30 +18,32 @@ client.once('ready', () => {
     console.log("Ready!");    
 });
 
-client.on('interactionCreate', interaction => {
-    interaction.deferReply()
-    .then(() => {
-        const { commandName } = interaction;
-        if(commandName === 'startquiz') {                
-            console.log('Um quiz foi iniciado por ' + interaction.user.username + '.');        
-            let question = [];
-            axios.get('http://3.145.101.81:3000/getrand')
-            .then(res => {
-                Object.assign(question, res.data);
-                let trivia = new Trivia(interaction, question);
-                setTimeout(() => {
-                    trivia.startTrivia();            
-                }, 100);
-            });
-        }
-        else if(commandName ==='status') {
-            axios.get('http://127.0.0.1:3000/howmany')
-            .then(res => {
-                let registered = res.data;
-                interaction.editReply( {content:'Bot ativo em ' + interaction.guild.name + '!\nPerguntas cadastradas: ' + registered.questions + '.', ephemeral: true} );
-            });        
-        }
-    });
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    const { commandName } = interaction;
+    if(commandName === 'startquiz') {                
+        await interaction.deferReply();
+        console.log('Um quiz foi iniciado por ' + interaction.user.username + '.');        
+        let question = [];
+        axios.get('http://3.145.101.81:3000/getrand')
+        .then(res => {
+            Object.assign(question, res.data);
+            let trivia = new Trivia(interaction, question);
+            setTimeout(() => {
+                trivia.startTrivia();            
+            }, 100);
+        });
+    }
+    else if(commandName ==='status') {
+        await interaction.deferReply();
+        axios.get('http://3.145.101.81:3000/howmany')
+        .then(res => {
+            let registered = res.data;
+            interaction.editReply( {content:'Bot ativo em ' + interaction.guild.name + '!\nPerguntas cadastradas: ' + registered.questions + '.', ephemeral: true} );
+        });        
+    }
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
